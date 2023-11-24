52597eba5b837c5748a1cc62ae9f0bfd3c5a864b
Alexander Revin <avrevin123@gmail.com>
2023-11-24 12:04:23 +0300

    FormPayment

--- src/components/Forms/AddPaymentForm/AddPaymentForm.js
+++ src/components/Forms/AddPaymentForm/AddPaymentForm.js
@@ -3,12 +3,7 @@ import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import ButtomBlue from './../../UI/Buttons/ButtonBlue'
-import Card from 'react-bootstrap/Card'
 
- import iconMir from './../../../assets/images/iconMir.svg'
-// import iconMastercard from './../../../assets/images/iconMastercard.svg'
-// import iconPaypal from './../../../assets/images/iconPaypal.svg'
-// import iconVisa from './../../../assets/images/iconVisa.svg'
 
 import './AddPaymentForm.css'
 
@@ -19,23 +14,18 @@ const AddPaymentForm = ({ active, setActive }) => {
     <div className={active ? "modal active" : "modal"}
       onClick={() => setActive(false)}>
       <Form onClick={(e) => e.stopPropagation()} className={active ? "form active" : "form"}>
-      <Col><h3>Новый способ оплаты</h3></Col>
+      <Col style={{borderBottomColor:'red'}}><h3>Новый способ оплаты</h3></Col>
         <Form.Group className='mb3'>
-          <Col><Form.Control placeholder='Номер карты' src= {iconMir}>
-              {/* <img src={iconMir}/> */}
-              {/* <img src={iconVisa}/>
-              <img src={iconMastercard}/>
-              <img src={iconPaypal}/> */}
-          </Form.Control></Col>
+          <Col><Form.Control placeholder='Номер карты' id="iconsPayment"/></Col>
           <Row style={{margin:'0'}}>
-            <Col><Form.Control placeholder='Срок действия карты' /></Col>
-            <Col><Form.Control placeholder='CVC' /></Col>
+            <Col><Form.Control placeholder='Срок действия карты'/></Col>
+            <Col><Form.Control placeholder='CVC' id="cvc"/></Col>
           </Row>
           <Col><Form.Control placeholder='Владелец карты'></Form.Control></Col>
-          <Col><Form.Control placeholder='Email'></Form.Control></Col>
+          <Col><Form.Control type="Email" placeholder='Email'></Form.Control></Col>
         </Form.Group>
         <Form.Group>
-        <Col><ButtomBlue>Добавить карту</ButtomBlue></Col>
+        <Col><ButtomBlue style={{float: 'right'}}>Добавить карту</ButtomBlue></Col>
         </Form.Group>
       </Form>
     </div>
